48431069496b585c78fe0e3c6543c662100bda72
brad@smithwebtek.com <brad@smithwebtek.com>
2021-07-06 22:32:43 -0400

    WIP210706-1032pm

--- lireddit-server/src/index.ts
+++ lireddit-server/src/index.ts
@@ -1,1 +1,24 @@
-console.log("hello there");
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+
+const main = async () => {
+  const orm = await MikroORM.init({
+    entities: [Post],
+    dbName: "lireddit",
+    type: "postgresql",
+    debug: !__prod__,
+    user: "deploy",
+    password: "pointer",
+  });
+
+  const post = orm.em.create(Post, { title: "my first post" });
+  await orm.em.persistAndFlush(post);
+
+  console.log("-------------------------sql2------------------");
+  await orm.em.nativeInsert(Post, { title: "my 2nd post" });
+};
+
+main().catch((err) => {
+  console.log("**********error**********: ", err);
+});
